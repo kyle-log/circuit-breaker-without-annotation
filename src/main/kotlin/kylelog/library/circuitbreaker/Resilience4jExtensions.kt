85b65393608b7a456b84bbe9f6a152fe0f0a785c
Kyle <cocomo1316@gmail.com>
2023-10-19 00:05:08 +0900

    Remove unnecessary comment

--- src/main/kotlin/kylelog/library/circuitbreaker/Resilience4jExtensions.kt
+++ src/main/kotlin/kylelog/library/circuitbreaker/Resilience4jExtensions.kt
@@ -3,9 +3,6 @@ package kylelog.library.circuitbreaker
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException
 import io.github.resilience4j.circuitbreaker.CircuitBreaker
 
-/**
- * Extensions for Resilience4j Dependencies
- */
 fun Throwable.turnToOpenExceptionIfOpen(): Throwable = when (this) {
     is CallNotPermittedException -> CircuitOpenException()
     else -> this
